d1eb46c7279be41aa93cac51206ad2dc0727339c
Greg Temchenko <soid.exe@gmail.com>
2012-05-02 19:03:41 -0700

    merge+some fb

--- ch11/AppDelegate.h
+++ ch11/AppDelegate.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FBConnect.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, FBSessionDelegate> {
+    Facebook *facebook;
+}
 
+@property (nonatomic, retain) Facebook *facebook;
 @property (strong, nonatomic) UIWindow *window;
 
 @end
